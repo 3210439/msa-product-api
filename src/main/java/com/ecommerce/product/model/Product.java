1d0afbe3698e62845bf5123499de57fbb90d8083
Bell_Bell <47379176+3210439@users.noreply.github.com>
2025-04-03 22:59:44 +0900

    엔티티에 기본적으로 createdAt 및 updatedAt 추가할 수 있도록 baseEntity 추가

--- src/main/java/com/ecommerce/product/model/Product.java
+++ src/main/java/com/ecommerce/product/model/Product.java
@@ -1,12 +1,16 @@
 package com.ecommerce.product.model;
 
+import com.ecommerce.product.model.common.BaseEntity;
 import lombok.Data;
 import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 @Entity
 @Table(name = "products")
-public class Product {
+public class Product extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
